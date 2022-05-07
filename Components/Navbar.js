7affa91912218876a98dea87c0f57a5aec234254
Abdallah <abdallahabouchahine@cmail.carleton.ca>
2022-05-06 20:03:59 -0400

    added about page and sample content

---
+++ Components/Navbar.js
@@ -0,0 +1,42 @@
+var html =
+	'<header class="nav-container">\
+			<nav>\
+				<a href="../index.html" class="nav-item name">Abdallah Abou-Chahine</a>\
+			</nav>\
+			<nav class="middle-content">\
+				<ul class="nav-elements projects">\
+					<li id="portfolio">\
+						<a href="" id="portfolio" class="nav-item link portfolio"\
+							>Portfolio</a\
+						>\
+					</li>\
+					<div class="drop-down-list" id="drop-down">\
+						<ul>\
+							<li><a href="">Graphic Design</a></li>\
+							<li><a href="">Art & Photography</a></li>\
+							<li><a href="">Technical Projects</a></li>\
+						</ul>\
+					</div>\
+				</ul>\
+			</nav>\
+\
+			<nav>\
+				<ul class="nav-elements personal">\
+					<li><a class="nav-item link" href="#">About</a></li>\
+					<li class="nav-item github">\
+						<a\
+							href="https://github.com/abdallahaac"\
+							class="nav-item link github"\
+							target="_blank"\
+							>Github</a\
+						>\
+					</li>\
+\
+					<a href="" id="contact-button" class="nav-item contact"\
+						>Contact me\
+					</a>\
+				</ul>\
+			</nav>\
+		</header>';
+
+export const navbar = (document.getElementById("nav").innerHTML = html);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+// Import Progress bar
 import { updateProgress, progressBar } from "../Components/Progress.js";
+// File: main.js
+// Import Highway
+// // Import Transitions
+// import Fade from "../Components/fade";
+
+// // Call Highway.Core once.
+// const H = new Highway.Core({
+// 	transitions: {
+// 		default: Fade,
+// 	},
+// });
+import { navbar } from "../Components/Navbar.js";
